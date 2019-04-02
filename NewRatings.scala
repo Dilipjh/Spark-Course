8e776f18cddc1fe4a918895cc6191f3639226387
Dilip Hiremath <djhiremath@gmail.com>
2019-04-02 17:37:12 +0200

    Add files via upload
    with latest mini data

---
+++ NewRatings.scala
@@ -0,0 +1,36 @@
+package SparkUdemyDi
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+
+object NewRatings {
+
+  println("Welcome to the Scala worksheet")
+  
+def main(args: Array[String]) {
+   
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+        
+    // Create a SparkContext using every core of the local machine, named RatingsCounter
+    val sc = new SparkContext("local[*]", "RatingsCounter")
+   
+    // Load up each line of the ratings data into an RDD
+    val lines = sc.textFile("../ml-latest-small/ratings.csv")
+    
+    // Convert each line to a string, split it out by tabs, and extract the third field.
+    // (The file format is userID, movieID, rating, timestamp)
+    val ratings = lines.map(x => x.toString().split(",")(2))
+    
+    // Count up how many times each value (rating) occurs
+    val results = ratings.countByValue()
+    
+    // Sort the resulting map of (rating, count) tuples
+    val sortedResults = results.toSeq.sortBy(_._1)
+    
+    // Print each result on its own line.
+    sortedResults.foreach(println)
+  }
+
+}
